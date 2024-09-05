01bd268e9df395ddafe74c868745a10f8fa54966
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-09-05 08:38:02 +0300

    add component lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
+import { Suspense, lazy, useState } from "react";
 import { Route, Routes, NavLink } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import MoviesPage from "./pages/MoviesPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
-import MovieReviews from "./components/MovieReviews/MovieReviews";
-import MovieCast from "./components/MovieCast/MovieCast";
 import "./App.css";
 
-//https://developer.themoviedb.org/docs/getting-started
-//API KEY: 6862d37066485f90139e04003f8d16a0
-//APIT TOKEN: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ODYyZDM3MDY2NDg1ZjkwMTM5ZTA0MDAzZjhkMTZhMCIsIm5iZiI6MTcyNDk5NTM2Ni44NzkzNDksInN1YiI6IjY2ZDE1Njg4Zjg0Nzg1YjVkMDZkZmVjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hSanP0oPdQ2_i0ijaBDTWOtCYLW_oKZYyBBarD8kA_w
-//
 function App() {
   const [count, setCount] = useState(0);
+  const HomePage = lazy(() => import("./pages/HomePage"));
+  const MoviesPage = lazy(() => import("./pages/MoviesPage"));
+  const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
+  const MovieReviews = lazy(() =>
+    import("./components/MovieReviews/MovieReviews")
+  );
+  const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
 
   return (
     <div className="App">
@@ -28,15 +26,17 @@ function App() {
           </ul>
         </nav>
       </header>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<MovieCast />} />
-          <Route path="reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<>Oops!</>} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<>Oops!</>} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
